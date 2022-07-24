e8efe910964a1dbe23efcb18562d11001c2824f9
Gayathrishetty007 <108149976+Gayathrishetty007@users.noreply.github.com>
2022-07-24 18:29:33 +0530

    Update FIFO.v

--- FIFO.v
+++ FIFO.v
@@ -1,96 +1,52 @@
 # Router1x3/FIFO
-module router_fifo(input [7:0] data_in,
-			input clk,resetn,write_enb,read_enb,lfd_state,soft_reset,
-			output reg [7:0] data_out,
-			output full,empty);
-	parameter width=9,
-		  depth=16,
-		  addr=5;
-	reg [width-1:0] wr_ptr,rd_ptr;
-	reg [width-1:0] mem [depth-1:0];
-	reg [addr:0] count;
-	reg lfd;
-	integer i;
+//RTL Code to generate FIFO
+module router_fifo #(parameter WIDTH = 8, DEPTH = 16, ADD_WIDTH = 4) 
+                    (input clock, resetn, write_enb, soft_reset, read_enb, lfd_state, 
+                     input [(WIDTH-1):0] data_in,
+		     output reg [(WIDTH-1):0] data_out,
+		     output empty, full);
+  
+  integer i;
+  reg [ADD_WIDTH:0] rd_ptr, wr_ptr;
+  reg [WIDTH:0] fifo_mem [(DEPTH-1):0]; 
+  reg [4:0] count;
+  reg temp;
+  
+  assign {full, empty} = {wr_ptr == {~rd_ptr[4], rd_ptr[3:0]}, rd_ptr == wr_ptr};
 
-always@(posedge clk)
-begin
-	if(~resetn)
-		lfd<=0;
-	else
-		lfd<=lfd_state;
-end
-
-always@(posedge clk)
-begin
-	if(~resetn)
-		data_out<=0;
-	else if (soft_reset)
-		data_out<=8'bz;
-	else if(count==0 && read_enb && empty)
-		data_out<=8'bz;
-	else
-	begin
-		if(read_enb && ~empty)
-			data_out<=mem[rd_ptr[3:0]][7:0];
-	end
-end
-
-always@(posedge clk)
-begin
-	if(~resetn)
-	begin
-		for(i=0;i<depth;i=i+1)
-			mem[i]<=0;
-	end
-	else if(soft_reset)
-	begin
-		for(i=0;i<depth;i=i+1)
-			mem[i]<=0;
-	end
-	else
-	begin
-		if(write_enb && ~full)
-			{mem[wr_ptr[3:0]][8],mem[wr_ptr[3:0]][7:0]}={lfd,data_in};
-	end
-end
-
-always@(posedge clk)
-begin
-	if(~resetn)
-	begin
-		wr_ptr<=0;
-		rd_ptr<=0;
-	end
-	else if(soft_reset)
-	begin
-		wr_ptr<=0;
-		rd_ptr<=0;
-	end
-	else
-	begin
-		if(write_enb && ~full)
-			wr_ptr<=wr_ptr+1'b1;
-		if(read_enb && ~empty)
-			rd_ptr<=rd_ptr+1'b1;
-	end
-end
-
-always@(posedge clk)
-begin
-	if(~resetn)
-		count<=0;
-	else if(read_enb && ~empty)
-	begin
-		if(mem[rd_ptr[3:0]][8])
-			count<=mem[rd_ptr[3:0]][7:2]+1'b1;
-		else if(count!=0)
-			count<=count-1;
-		else
-			count<=0;
-	end
-end
-
-assign full=(wr_ptr[4]==~rd_ptr[4] && wr_ptr[3:0]==rd_ptr[3:0])?1'b1:1'b0;
-assign empty=(wr_ptr == rd_ptr);
-
+  always@(posedge clock)
+    begin
+      temp <= (~resetn) ? 1'b0 : (soft_reset) ? 1'b0 : lfd_state;
+      if(!resetn) // Resets the FIFO and the related ports
+        begin
+	      for(i=0; i<DEPTH; i=i+1)		
+	        fifo_mem[i] <= 0;
+	      {count, rd_ptr, wr_ptr, data_out} <= {5'b0, 5'b0, 5'b0, 8'b0};
+	    end
+      else if(soft_reset)
+	    begin
+          for(i=0; i<DEPTH; i=i+1)
+	        fifo_mem[i] <= 0;
+	      {count, rd_ptr, wr_ptr, data_out} <= {5'b0, 5'b0, 5'b0, 8'bz};
+	    end
+	  else
+	    begin
+	      if(write_enb && !full) // Performs write operation on FIFO
+	        begin
+	          fifo_mem[wr_ptr[3:0]] <= {temp, data_in};
+                wr_ptr <= wr_ptr + 1;
+	        end
+	      if(!empty) // Performs read operation on FIFO
+	        begin
+		      if(read_enb)
+	   	        begin
+	     	     //Checking if the lfd_state value for payload_data is 0 or not
+	      	     {count, data_out} <= {(~fifo_mem[rd_ptr[3:0]][WIDTH]) ? (count > 0) * (count - 1) : fifo_mem[rd_ptr[3:0]][7:2] + 1, fifo_mem[rd_ptr[3:0]][(WIDTH-1):0]};
+	      	     rd_ptr <= rd_ptr + 1;
+	    	    end
+ 	        end
+	      else
+		    data_out <= 8'bz;
+        end
+    end
 endmodule
