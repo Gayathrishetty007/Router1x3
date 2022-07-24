5880906abac2cd45ef1f25be468d1ae87edb6730
Gayathrishetty007 <108149976+Gayathrishetty007@users.noreply.github.com>
2022-07-24 20:55:15 +0530

    Update Synchronizer.v

--- Synchronizer.v
+++ Synchronizer.v
@@ -1,127 +1,42 @@
-#Router1x3
-module router_sync(input clk,resetn,detect_add,full_0,full_1,full_2,empty_0,empty_1,empty_2,write_enb_reg,read_enb_0,read_enb_1,read_enb_2,
-			input [1:0] data_in,
-			output reg [2:0] write_enb,
-			output reg fifo_full,
-			output vld_out_0,vld_out_1,vld_out_2,
-			output reg soft_reset_0,soft_reset_1,soft_reset_2);
-	reg [1:0] temp;
-	reg [4:0] timer_0,timer_1,timer_2;
+//RTL for Router1x3_Synchronizer
+module router_sync(input [1:0] data_in,
+                   input detect_add, write_enb_reg, clock, resetn, read_enb_0, read_enb_1, read_enb_2, full_0, full_1, full_2, empty_0, empty_1, empty_2,
+				   output vld_out_0, vld_out_1, vld_out_2,
+				   output reg [2:0] write_enb,
+				   output reg soft_reset_0, soft_reset_1, soft_reset_2, fifo_full);
 
-always@(posedge clk)
-begin
-	if(~resetn)
- 		temp<=0;
- 	else if(detect_add)
- 		temp<=data_in;
-end
-
-always@(*)
-begin
- 	fifo_full=0;
- 	case(temp)
- 		2'b00 :fifo_full=full_0;
- 		2'b01 :fifo_full=full_1;
- 		2'b10 :fifo_full=full_2;
- 	endcase
-end
-
-always@(*)
-begin
- 	if(write_enb_reg)
+  reg [1:0] address_register;
+  reg [4:0] counter1, counter2, counter3;
+  
+  function [5:0] check_for_read_enable_signal(input re, soft_reset, input [4:0] counter);
 	begin
-		write_enb=0;
-		case(temp)
-			2'b00 :write_enb=3'b001; //hardcode
-			2'b01 :write_enb=3'b010;
-			2'b10 :write_enb=3'b100;
-		endcase
+	  counter = (re || counter == 5'd30) ? 5'b00000 : (counter + 1);
+	  soft_reset = (counter < 5'd30) ? 1'b0 : 1'b1;
+	  check_for_read_enable_signal = {soft_reset, counter};
 	end
-end
+  endfunction
 
-always@(posedge clk)
-begin
-	if(~resetn)
-		timer_0<=0;
-	else if(vld_out_0)
-	begin
-		if(read_enb_0)
-		begin
-			timer_0<=0;
-			soft_reset_0<=0;
-		end
-		else
-		begin
-			if(timer_0==30)
-			begin
-				timer_0<=0;
-				soft_reset_0<=1;
-			end
-			else
-			begin
-				timer_0<=timer_0+1;
-				soft_reset_0<=0;
-			end
-		end
+  assign {vld_out_0, vld_out_1, vld_out_2} = {~empty_0, ~empty_1, ~empty_2};
+  
+  always@(posedge clock) // sequential logic
+    begin
+	  address_register <= (~resetn) ? 2'b00 : (detect_add) ? data_in : address_register;
+	  case(address_register)
+	    0 : {soft_reset_0, counter1} <= (~resetn) ? 2'b00 : (vld_out_0) ? check_for_read_enable_signal(read_enb_0, soft_reset_0, counter1) : {soft_reset_0, counter1};
+	    1 : {soft_reset_1, counter2} <= (~resetn) ? 2'b00 : (vld_out_1) ? check_for_read_enable_signal(read_enb_1, soft_reset_1, counter2) : {soft_reset_1, counter2};
+	    2 : {soft_reset_2, counter3} <= (~resetn) ? 2'b00 : (vld_out_2) ? check_for_read_enable_signal(read_enb_2, soft_reset_2, counter3) : {soft_reset_2, counter3};
+		default: {soft_reset_0, soft_reset_1, soft_reset_2, counter1, counter2, counter3} <= 18'd0;
+	  endcase
 	end
-end
 
-always@(posedge clk)
-begin
-	if(~resetn)
-		timer_1<=0;
-	else if(vld_out_1)
-	begin
-		if(read_enb_1)
-		begin
-			timer_1<=0;
-			soft_reset_1<=0;
-		end
-		else
-		begin
-			if(timer_1==30)
-			begin
-				timer_1<=0;
-				soft_reset_1<=1;
-			end
-			else
-			begin
-				timer_1<=timer_1+1;
- 				soft_reset_1<=0;
- 			end
-		end
+  always@(*) // combinational logic
+    begin
+	  {fifo_full, write_enb} = 4'b0xxx;
+	  case(address_register)
+	    	0: {fifo_full, write_enb} = {full_0, (~resetn) ? 3'b000 : (write_enb_reg) ? 3'b001: 3'b000};
+		1: {fifo_full, write_enb} = {full_1, (~resetn) ? 3'b000 : (write_enb_reg) ? 3'b010: 3'b000};
+		2: {fifo_full, write_enb} = {full_2, (~resetn) ? 3'b000 : (write_enb_reg) ? 3'b100: 3'b000};
+	  endcase
 	end
-end
-
-always@(posedge clk)
-begin
-	if(~resetn)
-		timer_2<=0;
-	else if(vld_out_2)
-	begin
-		if(read_enb_2)
-		begin
-			timer_2<=0;
-			soft_reset_2<=0;
-		end 
-		else
- 		begin
-			if(timer_2==30)
-			begin
-				timer_2<=0;
-				soft_reset_2<=1;
- 			end
-			else
-			begin
-				timer_2<=timer_2+1;
-				soft_reset_2<=0;
- 			end
-		end
-	end
-end
-
-assign vld_out_0=~empty_0;
-assign vld_out_1=~empty_1;
-assign vld_out_2=~empty_2;
 endmodule
 
